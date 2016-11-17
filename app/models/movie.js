c5628ea4e33d9b66680041cbf9ad84ee5ac75270
Egor Gorbunov <egor-mailbox@ya.ru>
2016-11-18 01:31:32 +0300

    Add server response support

--- app/models/movie.js
+++ app/models/movie.js
@@ -1,11 +1,35 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-	movieId: DS.attr(), // link to imdb
+	movieId: DS.attr(),
 	title: DS.attr(),
 	plot: DS.attr(), // tiny plot
 	date: DS.attr(),
 	poster: DS.attr(),
 	fromReviews: DS.attr(),
-	fragment: DS.attr() // piece where match was find
+	fragment: DS.attr(), // piece where match was find
+
+	cuttedPlot: Ember.computed('plot', function() {
+		var plot = this.get('plot')
+		const maxLen = 500;
+		if (plot.length < maxLen) {
+			return plot;
+		} else {
+			return `${plot.substring(0, maxLen)}...`;
+		}
+    }),
+
+    notNullPoster: Ember.computed('poster', function() {
+    	if (this.get('poster') === undefined) {
+    		return "https://upload.wikimedia.org/wikipedia/commons/3/37/No_person.jpg";
+    	}
+    	return this.get('poster');
+    }),
+
+    notNullDate: Ember.computed('date', function() {
+    	if (this.get('date') === undefined || this.get('date').length == 0) {
+    		return "-";
+    	}
+    	return this.get('date');
+    })
 });
